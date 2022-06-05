396d8876e7e0b4456200d6d59397a3fab3e6c0d9
MarT <MarT@192.168.56.1>
2022-06-05 11:19:15 +0300

    Askisi 4 _ Lab07

--- rest.example/src/main/java/gr/upatras/rest/example/Application.java
+++ rest.example/src/main/java/gr/upatras/rest/example/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-// Added new lines for new push to develop
+// Added new lines for new push to develop 
 @SpringBootApplication
 public class Application {
 
